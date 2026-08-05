cfdd41835ecc694b0c10229228e44cca1a29737b
hammad-x25 <contact.hammadishfaq@gmail.com>
2026-08-05 23:27:08 +0500

    custome response

---
+++ src/utils/apiresponse.js
@@ -0,0 +1,10 @@
+class apiresponse 
+{
+    constructor(statuscode,message="success",data)
+    {
+        this.statuscode=statuscode;
+        this.message=message;
+        this.data=data;
+        this.success=statuscode<400;
+    }
+}
